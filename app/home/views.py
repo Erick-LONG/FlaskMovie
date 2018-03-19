059965665c62d05bc94207406dddeb0f2ecb4653
fulong <834424581@qq.com>
2018-03-19 13:23:31 +0800

    登录页面搭建

--- app/home/views.py
+++ app/home/views.py
@@ -1,6 +1,23 @@
 from . import home #导入蓝图对象
-from flask import render_template
+from flask import render_template,redirect,url_for
+
+
 
 @home.route('/')
 def index():
     return render_template('home/index.html')
+
+
+@home.route('/login/')
+def login():
+    return render_template('home/login.html')
+
+
+@home.route('/logout/')
+def logout():
+    return redirect(url_for('home.login'))
+	
+
+@home.route('/regist/')
+def regist():
+    return render_template('home/regist.html')
