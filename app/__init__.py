181b482814aed7c028082cca52581aaee94d6d31
erick <834424581@qq.com>
2018-03-24 10:53:42 +0800

    管理员登录表单验证

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,20 @@
 from flask import Flask,render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@127.0.0.1:3306/flaskmovie'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['SECRET_KEY']='0b378e581f3c402da694e3c77f318889'
 app.debug = True
+db = SQLAlchemy(app)
+
 
 from app.home import home as home_blueprint
 from app.admin import admin as admin_blueprint
 
 app.register_blueprint(home_blueprint)
 app.register_blueprint(admin_blueprint, url_prefix='/admin')
+
 
 @app.errorhandler(404)
 def page_not_find(error):
--- app/models.py
+++ app/models.py
@@ -1,12 +1,5 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@127.0.0.1:3306/flaskmovie'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-
-db = SQLAlchemy(app)
+from app import db
 
 
 # 会员
@@ -51,6 +44,7 @@ class Tag(db.Model):
 
     def __repr__(self):
         return '<Tag %r>' % self.name
+
 
 # 电影
 class Movie(db.Model):
@@ -152,6 +146,10 @@ class Admin(db.Model):
     def __repr__(self):
         return '<Admin %r>' % self.name
 
+    def check_pwd(self,pwd):
+        from werkzeug.security import check_password_hash
+        return check_password_hash(self.pwd,pwd)
+
 
 # 管理员登录日志
 class Adminlog(db.Model):
@@ -178,22 +176,22 @@ class Oplog(db.Model):
         return '<Oplog %r>' % self.id
 
 
-if __name__ == '__main__':
-    #db.create_all()
-
-    # role = Role(
-    #     name = "超级管理员",
-    #     auths = '',
-    #     )
-    # db.session.add(role)
-    # db.session.commit()
-
-    from werkzeug.security import generate_password_hash
-    admin = Admin(
-        name = 'imoocmovie1',
-        pwd = generate_password_hash('imoocmovie1'),
-        is_super = 0,
-        role_id = 1,
-        )
-    db.session.add(admin)
-    db.session.commit()
+        # if __name__ == '__main__':
+        # #db.create_all()
+        #
+        # # role = Role(
+        # #     name = "超级管理员",
+        # #     auths = '',
+        # #     )
+        # # db.session.add(role)
+        # # db.session.commit()
+        #
+        # from werkzeug.security import generate_password_hash
+        # admin = Admin(
+        #     name = 'imoocmovie1',
+        #     pwd = generate_password_hash('imoocmovie1'),
+        #     is_super = 0,
+        #     role_id = 1,
+        #     )
+        # db.session.add(admin)
+        # db.session.commit()
