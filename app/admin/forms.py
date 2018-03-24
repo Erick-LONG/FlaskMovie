5396152528bc7d1a7feb14a861efe8b16b45df3c
erick <834424581@qq.com>
2018-03-24 12:22:01 +0800

    电影标签CURD,及分页

--- app/admin/forms.py
+++ app/admin/forms.py
@@ -3,6 +3,7 @@ from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired,ValidationError
 from app.models import Admin
 
+
 class LoginForm(FlaskForm):
     '''管理员登录表单'''
     account = StringField(
@@ -41,3 +42,25 @@ class LoginForm(FlaskForm):
         admin = Admin.query.filter_by(name = account).count()
         if admin==0:
             raise ValidationError('账号不存在')
+
+
+class TagForm(FlaskForm):
+    name = StringField(
+        label='名称',
+        validators=[
+            DataRequired('请输入标签')
+        ],
+        description='标签',
+        render_kw={
+            "class":"form-control",
+            'id':"input_name",
+            "placeholder":"请输入标签名称",
+        }
+    )
+
+    submit = SubmitField(
+        label="编辑",
+        render_kw={
+            "class": "btn btn-primary",
+        }
+    )
